b156a5b597301e08bb23b1cdfe322cad669ac1d7
Alexander Ishchenko <qialex.i@gmail.com>
2017-06-06 05:04:33 +0200

    in progress

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Player } from './model/player';
 import { Game } from './model/game';
 import { Router, NavigationStart } from '@angular/router';
+import { Square } from './model/square';
 import 'rxjs/add/operator/filter';
 
 @Injectable()
@@ -24,17 +25,17 @@ export class PlayerService {
         });
   }
   private opponentAI() {
-    const targets: any = [];
-    const firedTargets: any = [];
-    let finalTarget: any;
+    const targets: Array<Square> = [];
+    const firedTargets: Array<Square> = [];
+    let finalTarget: Square;
 
-    this.player.field.map((row: any) => row.map((square: any) => {
+    this.player.field.map((row: Array<Square>) => row.map((square: Square) => {
         if (square.isFired && square.isShip) {
-            for (let k = 0;  k < square.allNeighbors.length; k++) {
-                if (!square.allNeighbors[k].isFired) {
-                    firedTargets.push(square.allNeighbors[k]);
+            square.allNeighbors.map((neighbor: Square) => {
+                if (!neighbor.isFired) {
+                    firedTargets.push(neighbor);
                 }
-            }
+            });
         }
         if (!square.isFired) {
             targets.push(square);
@@ -44,6 +45,8 @@ export class PlayerService {
     if (firedTargets.length) {
         finalTarget = firedTargets[Math.floor(Math.random() * firedTargets.length)];
     } else {
+        finalTarget = targets[Math.floor(Math.random() * targets.length)];
+        /*
         const filteredTargets: any = [];
         let maximunUnfuredNeighbors = 0;
         for (let i = 0; i < targets.length; i++) {
@@ -69,11 +72,12 @@ export class PlayerService {
         }
 
         finalTarget = filteredTargets[Math.floor(Math.random() * filteredTargets.length)];
+        */
     }
 
     return finalTarget;
   }
-  public fire (square: any) {
+  public fire (square: Square) {
       if (!this.game.isHumanTurn || square.isFired || this.opponent.allShipsAreOnFire() || this.player.allShipsAreOnFire() ) {
           return;
       }
